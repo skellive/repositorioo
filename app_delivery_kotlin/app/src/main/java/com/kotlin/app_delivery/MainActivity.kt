399110218ad177662c7f093c83fbcd49d37166ee
iabotprueba <63275364+iabotprueba@users.noreply.github.com>
2020-06-06 14:25:21 -0500

    commit
    validaciones

--- app_delivery_kotlin/app/src/main/java/com/kotlin/app_delivery/MainActivity.kt
+++ app_delivery_kotlin/app/src/main/java/com/kotlin/app_delivery/MainActivity.kt
@@ -1,9 +1,17 @@
 package com.kotlin.app_delivery
 
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_register_page.*
 
+
+enum class ProviderType{
+    BASIC
+}
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,8 +24,76 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        setup()
+
+
+    }
+    private fun setup(){
+
+        txt_registro.setOnClickListener {
+            val registerIntent = Intent( this, register_page:: class.java)
+            startActivity(registerIntent)
+        }
+
+        button2_login.setOnClickListener {
+            if (editTextTextPersonName.text.isNotEmpty() && editTextTextPassword.text.isNotEmpty()) {
+
+                    FirebaseAuth.getInstance()
+                        .signInWithEmailAndPassword(editTextTextPersonName.text.toString(),
+                            editTextTextPassword.text.toString()).addOnCompleteListener {
+                            if (it.isSuccessful){
+                                showLogin(it.result?.user?.email ?:"", ProviderType.BASIC)
+
+                            }else{
+                                showAlertAutentic()
+                            }
+                        }
+
+
+
+
+            } else {
+                showAlertCamposVacios()
+            }
+        }
+    }
+
+
+    private fun showLogin(email: String, provider: ProviderType){
+        val MainIntent = Intent(this, MainActivity::class.java).apply {
+            putExtra("email", email)
+            putExtra("provider", provider.name)
+        }
+        startActivity(MainIntent)
+
 
     }
 
 
-}
+    private fun showAlertCamposVacios(){
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Error")
+        builder.setMessage("Campos vacios. Por favor llene los campos correspondientes")
+        builder.setPositiveButton("Aceptar", null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+
+    }
+
+
+    private fun showAlertAutentic(){
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Error")
+        builder.setMessage("Se ha producido un error al registrar o auntenticar al usuario")
+        builder.setPositiveButton("Aceptar", null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+
+    }
+
+
+    }
+
+
+
+
--- app_delivery_kotlin/app/src/main/java/com/kotlin/app_delivery/register_page.kt
+++ app_delivery_kotlin/app/src/main/java/com/kotlin/app_delivery/register_page.kt
@@ -1,12 +1,93 @@
 package com.kotlin.app_delivery
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_register_page.*
 
 
 class register_page : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register_page)
+
+        setup()
     }
+
+    private fun setup() {
+
+        title = "Autenticacion"
+        button_continuar_registro.setOnClickListener {
+            if (editTextTextPersonName4.text.isNotEmpty() && editTextTextPassword2.text.isNotEmpty() && editTextTextPassword3.text.isNotEmpty()) {
+                if (editTextTextPassword2.text.toString()
+                        .equals(editTextTextPassword3.text.toString())
+                ) {
+                    FirebaseAuth.getInstance()
+                        .createUserWithEmailAndPassword(editTextTextPersonName4.text.toString(),
+                            editTextTextPassword2.text.toString()).addOnCompleteListener {
+                            if (it.isSuccessful){
+                                    showLogin(it.result?.user?.email ?:"", ProviderType.BASIC)
+
+                            }else{
+                                showAlertAutentic()
+                            }
+                        }
+
+
+                } else {
+                    showAlertContNoCoinciden()
+                }
+
+            } else {
+                showAlertCamposVacios()
+            }
+        }
+    }
+
+
+    private fun showLogin(email: String, provider: ProviderType){
+        val MainIntent = Intent(this, MainActivity::class.java).apply {
+            putExtra("email", email)
+            putExtra("provider", provider.name)
+        }
+        startActivity(MainIntent)
+
+
+    }
+
+
+    private fun showAlertCamposVacios(){
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Error")
+        builder.setMessage("Campos vacios. Por favor llene los campos correspondientes")
+        builder.setPositiveButton("Aceptar", null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+
+    }
+
+    private fun showAlertContNoCoinciden(){
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Error")
+        builder.setMessage("Las contraseñas no coinciden")
+        builder.setPositiveButton("Aceptar", null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+
+    }
+
+    private fun showAlertAutentic(){
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Error")
+        builder.setMessage("Se ha producido un error al registrar o auntenticar al usuario")
+        builder.setPositiveButton("Aceptar", null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+
+    }
+
+
 }
