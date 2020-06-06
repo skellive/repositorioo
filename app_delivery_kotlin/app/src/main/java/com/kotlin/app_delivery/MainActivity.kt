f61d6be8a8c942de8cf44cd135aeb3071fd55987
iabotprueba <63275364+iabotprueba@users.noreply.github.com>
2020-06-05 19:57:31 -0500

    commit
    as

--- app_delivery_kotlin/app/src/main/java/com/kotlin/app_delivery/MainActivity.kt
+++ app_delivery_kotlin/app/src/main/java/com/kotlin/app_delivery/MainActivity.kt
@@ -2,10 +2,14 @@ package com.kotlin.app_delivery
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 
 
 class MainActivity : AppCompatActivity() {
+
+    private var mAuth: FirebaseAuth? = null
     override fun onCreate(savedInstanceState: Bundle?) {
+        mAuth = FirebaseAuth.getInstance()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
